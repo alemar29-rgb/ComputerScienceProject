a43a4aef72862e6ad116a1a9dfc1ddedea83436b
Alexander <alemar29@bergen.org>
2026-05-08 14:55:05 -0400

    5/8/26 early commit since computer is about to die, added drop down menu with CSS/JS and added Related Songs HTML/CSS
    Co-authored-by: prince <andcos29@users.noreply.github.com>

--- javascript/script.js
+++ javascript/script.js
@@ -2,9 +2,9 @@
   // 1. Grab all the buttons and all the content sections
   const buttons = document.querySelectorAll('.navBar button');
   const sections = [
-    document.querySelector('.related_artist'),
-    document.querySelector('.mood_song'),
-    document.querySelector('.related_songs')
+    document.querySelector('.related-artist'),
+    document.querySelector('.mood-song'),
+    document.querySelector('.related-songs')
   ];
 
   // 2. Loop through the buttons
@@ -20,3 +20,15 @@
       sections[index].classList.remove('hidden');
     });
   });
+
+  // DROPDOWN MENU / Doesnot work yet
+
+  // 1. Target the button and the menu
+const dropdownBtn = document.getElementById('dropdownMenuButton')
+const dropdownMenu = document.querySelector('.dropdownMenu')
+
+// 2. Toggle the menu when the button is clicked
+dropdownBtn.addEventListener('click', (e) => {
+    dropdownMenu.classList.toggle('show-menu')
+})
+
